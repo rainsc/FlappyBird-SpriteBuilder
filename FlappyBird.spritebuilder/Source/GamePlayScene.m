7d3c30902ff0043f01bc90fa100edcae58e4d428
rainsc <rainsc@calhounschools.org>
2015-02-23 10:56:49 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -16,5 +16,8 @@
 }
 
 // put new methods here
-
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+}
 @end
